3b3a08fe4afdaf8595cb8d455b7291fc066474c1
moreiraandrep <moreira.andre.p@gmail.com>
2021-10-21 22:07:50 -0300

    Crachar virtual

---
+++ main.js
@@ -0,0 +1,38 @@
+const userSocialMedia = {
+  github: 'moreiraandrep',
+  youtube: 'c/RocketSeat',
+  facebook: 'andre.moreiradev',
+  instagram: 'andre_moreirap',
+  twitter: 'andre_moreirap'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const URL_BASE = `https://api.github.com/users/${userSocialMedia.github}`
+
+  fetch(URL_BASE)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      userName.textContent = data.name
+
+      userAvatar.src = data.avatar_url
+
+      userLogin.href = data.html_url
+      userLogin.children[1].textContent = data.login
+
+      userBio.textContent = data.bio
+    })
+}
+
+getGitHubProfileInfos()
